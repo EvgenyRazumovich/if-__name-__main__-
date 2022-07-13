582a6d158f158481b551813f9c42c92bff1c7c66
Ваше Имя <z7758@mail.ru>
2022-07-13 12:22:16 +0300

    created main

---
+++ goodbyes.py
@@ -0,0 +1,10 @@
+import greeting
+
+greeting.func()
+
+print('hi i am second print')
+
+if __name__ == '__main__':
+    print('i am second main')
+else:
+    print('i am second is not import')
---
+++ greeting.py
@@ -0,0 +1,10 @@
+def func():
+    print('hi im func')
+
+
+print('hi i am first print')
+
+if __name__ == '__main__':
+    print('i am first name and i is not import')
+else:
+    print('i am import first name')
